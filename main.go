e3070e9ecf859e1c11792ec4750cf4f680004ce1
deathcore666 <fr.niez@gmail.com>
2018-02-08 16:23:18 +0600

    yolo

--- main.go
+++ main.go
@@ -10,12 +10,13 @@ import (
 	"bufio"
 	"fmt"
 	"regexp"
+	"strconv"
 )
 
 type Country struct {
 	Name string `json:"name"`
-	Code string `json:"code"`
-	GPD  int64  `json:"GPD"`
+	Code []string `json:"code"`
+	GdpPercapita  float64  `json:"GdpPerCapita"`
 }
 
 func main() {
@@ -35,29 +36,31 @@ func main() {
 	runes := []rune(s)
 	resultString := string(runes[openTagIndex:closingTagIndex])
 
-	parseTable(resultString)
-	//if err != nil {
-	//	panic(err)
-	//}
+	err = parseTable(resultString)
+	if err != nil {
+		panic(err)
+	}
 }
 
-func parseTable(input string) {
+func parseTable(input string) error {
 	finishIndex := strings.Index(input, "</tr>")
 	if finishIndex == -1 {
-		return
+		return nil
 	}
 	finishIndex += 4
 
-	//var myCountry Country
+	var myCountry Country
 	nameStart := strings.Index(input, "\">") + 2
-	nameEnd:= strings.Index(input, "</a>")
-
-	runes := []rune(input)
-	countryName := string(runes[nameStart:nameEnd])
+	nameEnd := strings.Index(input, "</a>")
+	if nameEnd == -1 {
+		return nil
+	}
+	countryName := input[nameStart:nameEnd]
 	log.Println(countryName)
 
 	input = input[nameEnd+8:]
 	countryCode, codeLastIndex := extractTagsData(input)
+	countryCodes := strings.Split(countryCode, ", ")
 	log.Println(countryCode)
 
 	input = input[codeLastIndex+5:]
@@ -66,7 +69,13 @@ func parseTable(input string) {
 
 	input = input[lettersLastIndex:]
 	countryPopulation, popLastIndex := extractTagsData(input)
-	log.Println(countryPopulation)
+	countryPopulation = strings.Replace(countryPopulation, ",", "", -1)
+	popFloat, err := strconv.ParseFloat(countryPopulation, 64)
+	if err != nil {
+		log.Println(err)
+		return err
+	}
+	log.Println(popFloat)
 
 	input = input[popLastIndex+5:]
 	areaReg := "<td>([0-9,])+</td>"
@@ -74,26 +83,58 @@ func parseTable(input string) {
 
 	input = input[areaLastIndex:]
 	countryGdp, _ := extractTagsData(input)
-	log.Println(countryGdp)
 
-	//myCountry.Name = countryName
-	//myCountry.Code = countryCode
-	//err := writeToFile(myCountry)
-	//if err != nil {
-	//	return err
-	//}
-	//
-	//return parseTable(string(runes[finishIndex:]))
+	var multiplier float64
+	if strings.Contains(countryGdp, "Billion") {
+		multiplier = 1000000000
+	}
+	if strings.Contains(countryGdp, "Million") {
+		multiplier = 1000000
+	}
+	if strings.Contains(countryGdp, "Trillion") {
+		multiplier = 1000000000000
+	}
+
+	countryGdp = strings.Replace(countryGdp, " Trillion", "", -1)
+	countryGdp = strings.Replace(countryGdp, " Billion", "", -1)
+	countryGdp = strings.Replace(countryGdp, " Million", "", -1)
+
+	if countryGdp == "" {
+		countryGdp = "0"
+	}
+	gdpFloat, err := strconv.ParseFloat(countryGdp, 64)
+	if err != nil {
+		log.Println(err)
+		return err
+	}
+	gdpFloat *= multiplier
+	log.Println(strconv.FormatFloat(gdpFloat, 'f', -1, 64))
+
+	myCountry.Name = countryName
+	myCountry.Code = countryCodes
+	myCountry.GdpPercapita = -1
+	if popFloat != 0 {
+		myCountry.GdpPercapita = gdpFloat / popFloat
+	}
+
+	err = writeToFile(myCountry)
+	if err != nil {
+		log.Println(err)
+		return err
+	}
+
+	return parseTable(input)
 }
 
 func extractTagsData(input string) (string, int) {
 	dataStart := strings.Index(input, "<td>") + 4
 	dataEnd := strings.Index(input, "</td>")
 	data := input[dataStart:dataEnd]
+	input = input[dataEnd+5:]
 	return data, dataEnd
 }
 
-func extractWithRegexp(input string, reg string)(string,int) {
+func extractWithRegexp(input string, reg string) (string, int) {
 	dataReg, _ := regexp.Compile(reg)
 	data := string(dataReg.Find([]byte(input)))
 	dataLastIndex := strings.Index(input, data) + len(data)
